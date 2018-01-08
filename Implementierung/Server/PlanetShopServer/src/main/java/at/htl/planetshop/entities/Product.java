951e3d823f5f75ba03258585174563f205f226ae
mazinator <33018106+mazinator@users.noreply.github.com>
2018-01-08 10:38:54 +0100

    try
    versuch

---
+++ Implementierung/Server/PlanetShopServer/src/main/java/at/htl/planetshop/entities/Product.java
@@ -0,0 +1,79 @@
+package at.htl.planetshop.entities;
+
+import javax.persistence.*;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.io.Serializable;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@NamedQueries({
+        @NamedQuery(name = "findAll",query = "select v from Product v")
+})
+@XmlRootElement
+public class Product implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private double price;
+    private String name;
+    @Lob
+    private byte[] image;
+
+    private String details;
+
+    public Product() {
+    }
+
+    //region Constructors
+    public Product(double price, String name) {
+        this.price = price;
+        this.name = name;
+    }
+
+    public Product(double price, String name, byte[] image, String details) {
+        this.price = price;
+        this.name = name;
+        this.image = image;
+        this.details = details;
+    }
+    //endregion
+
+    //region Getter Setter
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+    //endregion
+}
+
